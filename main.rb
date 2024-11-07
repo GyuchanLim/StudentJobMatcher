75280f629aaec6a2775c977e478d86c6258c635a
Gyuchan Lim <gyuchantech@gmail.com>
2024-11-07 22:07:07 +1300

    Make use of methods
    Cleaned up code a bit
    
    Need to
    - improve output handling
    - implement some sorting
    - try to work with some ivars?

--- main.rb
+++ main.rb
@@ -1,29 +1,48 @@
 require 'csv'
+require 'debug'
 
-def main
-    # id,name,skills
-    arr_of_jobseekers_rows = CSV.read("input/jobseekers.csv", headers: :first_row)
+module  StudentJobMatcher
+    class  Processer
+        attr_reader :job_seekers_csv, :jobs_csv
 
-    # id,title,required_skills
-    arr_of_jobs_rows = CSV.read("input/jobs.csv", headers: :first_row)
-
-    arr_of_jobseekers_rows.each do |jobseeker|
-        matching_jobs = []
-
-        puts "Processing #{jobseeker["name"]}, id: #{jobseeker["id"]}"
-        arr_of_jobs_rows.each do |jobs|
-            matching_jobs.append(match_skills(jobseeker["skills"], jobs["required_skills"]))
+        def initialize(job_seekers_csv, jobs_csv)
+            @job_seekers_csv = job_seekers_csv
+            @jobs_csv = jobs_csv
         end
 
-        pp matching_jobs
+        def start
+            job_seekers_csv.each do |jobseeker|
+                puts "Processing #{jobseeker["name"]}, id: #{jobseeker["id"]}"
+                pp process_jobseeker(jobseeker)
+            end
+        end
+
+        def process_jobseeker(jobseeker)
+            jobseeker_output = []
+
+            jobs_csv.each do |job|
+                seeker_skills = jobseeker["skills"].split(', ')
+                required_skills = job["required_skills"].split(', ')
+
+                matched_skills = match_skills(seeker_skills, required_skills)
+
+                unless matched_skills.nil?
+                    jobseeker_output.append([jobseeker["id"], jobseeker["name"], job["id"], job["title"], matched_skills.count, format_matched_percentage(matched_skills, required_skills) ])
+                end
+            end
+
+            jobseeker_output
+        end
+
+        def match_skills(seeker_skills, required_skills)
+            matched_skills = seeker_skills.intersection(required_skills)
+            matched_skills.empty? ? nil : matched_skills
+        end
+
+        def format_matched_percentage(matched_skills, required_skills)
+            ((matched_skills.count.to_f/required_skills.count)*100).round
+        end
     end
 end
 
-def match_skills(jobseeker, jobs)
-    seeker_skills = jobseeker.split(', ')
-    required_skills = jobs.split(', ')
-
-    seeker_skills.intersection(required_skills)
-end
-
-main
+StudentJobMatcher::Processer.new(CSV.read("input/jobseekers.csv", headers: :first_row), CSV.read("input/jobs.csv", headers: :first_row)).start
